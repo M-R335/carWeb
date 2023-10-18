e0dc0f34335d933ba08ae829f844b4e6abecb791
unknown <rodriguezmark321@gmail.com>
2023-10-18 01:28:56 -0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    compiler: {
+        styledComponents: true,
+    },
+    images:{
+        unoptimized: true,
+        domains:['media.graphassets.com']
+    }
+}
+
+module.exports = nextConfig
+
+
+
+
+
+
